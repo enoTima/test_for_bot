b85e05ebcc60f7f7e96a20730b64d9a5b96229ff
enoTima <pogodiv03@gmail.com>
2023-02-09 19:54:37 +0200

    commit

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from aiogram import Bot, Dispatcher, types, executor
 import logging
 import time
+import gspread
+import datetime
 
 logging.basicConfig(filename="sample.log", level=logging.INFO)
 API_TOKEN = '6131336099:AAFjWhsFH-FpQNoz8uzEQf1BuNf36YN7TL8'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+sa = gspread.service_account(filename="serv_acc.json")
+sh = sa.open("Осколки Е&Е")
+wks = sh.worksheet("Аркуш4")
 
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Hi!")
-    logging.info(f'id: {message.from_user.id}; '
-                 f'full_name: {message.from_user.full_name}; '
-                 f'time: {time.asctime()} {message.text}')
 
 
 @dp.message_handler(commands=['help'])
@@ -30,7 +32,11 @@ async def adding_shards(message: types.Message):
 
 @dp.message_handler(commands=['add'])
 async def adding_shards(message: types.Message):
-    await message.answer(message.text.split(' ')[1:])
+    answer_list = message.text.split(' ')[1:]
+    d = datetime.date.today()
+    wks.append_row([f'{d.day}.{d.month}.{d.year}', message.from_user.id,
+                    answer_list[2], answer_list[0], answer_list[1]])
+
 
 if __name__ == "__main__":
     executor.start_polling(dp)
